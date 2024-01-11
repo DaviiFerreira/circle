a926990adc3b44baf0db184530db90201621e2fd
Davi Ferreira <davi.medley@hotmail.com>
2024-01-11 20:04:04 -0300

    contador de pontos n funfa mas calcula

--- source.js
+++ source.js
@@ -2,6 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     var fundo = document.getElementById('fundo');
     var desenhando = false; // Variável para rastrear se o botão está sendo mantido pressionado
     var iniciado = false;
+    var ymeio = fundo.clientHeight/2;
+    var xmeio = fundo.clientWidth/2;
+    var posicoesMouse= [];
+
     fundo.addEventListener('mousedown', function(event) {
         // Verifica se o botão esquerdo do mouse foi pressionado
         if (event.button === 0 && iniciado) {
@@ -38,7 +42,12 @@ document.addEventListener('DOMContentLoaded', function() {
             // Cria um elemento de linha
             var trail = document.createElement('div');
             trail.classList.add('trail');
-            console.log()
+            posicoesMouse.push( (((event.clientX-xmeio)**2) +((event.clientY-ymeio+3)**2))**0.5 );
+            if (posicoesMouse.length > 100) {
+                posicoesMouse.shift(); // Remove a posição mais antiga se houver mais de 100
+            }
+            console.log("posx, posy, meiox, meioy", event.clientX, event.clientY, xmeio, ymeio)
+            console.log(posicoesMouse);
             // Define a posição inicial da linha
             trail.style.position = 'absolute';
             trail.style.left = event.clientX + 'px';
