ae75d381e9b3c1ec78476220bcdf6da1f622bd8e
Davi Ferreira <davi.medley@hotmail.com>
2024-01-15 17:30:59 -0300

    modified:   circle.html 	modified:   source.js

--- source.js
+++ source.js
@@ -5,15 +5,45 @@ document.addEventListener('DOMContentLoaded', function() {
     var ymeio = fundo.clientHeight/2;
     var xmeio = fundo.clientWidth/2;
     var posicoesMouse= [];
-
+    var xInicial = 0;
+    var yInicial = 0;
+    var posInit = true ; 
+    var xSeg = 0;
+    var ySeg = 0;
+    var posSeg = true ; 
+    var estoura =0;
+    var horario = 0;
+    var quadrante = 0;
+    var dezEmDez = 0;
     fundo.addEventListener('mousedown', function(event) {
         // Verifica se o botão esquerdo do mouse foi pressionado
         if (event.button === 0 && iniciado) {
             desenhando = true; // Começa a desenhar
+            var pontos= document.getElementById('pontos');
+            if( pontos.innerHTML[0] == 's'   ){
+                pontos.innerHTML  = '100.00';
+                posInit = true; 
+                posSeg = true;
+                while(posicoesMouse.length){
+                    posicoesMouse.shift();
+                }
+                estoura=0;
+            }
+            if( pontos.innerHTML[0] == 'd'   ){
+                pontos.innerHTML  = '100.00';
+                posInit = true; 
+                posSeg = true;
+                while(posicoesMouse.length){
+                    posicoesMouse.shift();
+                }
+                estoura=0;
+            }
+           
+           // posInit = true;
         }
     });
 
-    fundo.addEventListener('mouseup', function(event) {
+    fundo.addEventListener('mouseup', function(event) {a
         // Verifica se o botão esquerdo do mouse foi solto
         if (event.button === 0 && iniciado) {
             desenhando = false; // Para de desenhar
@@ -40,12 +70,18 @@ document.addEventListener('DOMContentLoaded', function() {
     fundo.addEventListener('mousemove', function(event) {
         // Verifica se o botão esquerdo do mouse está pressionado
         if (desenhando) {
+            estoura++;
             // Cria um elemento de linha
             var trail = document.createElement('div');
             trail.classList.add('trail');
             posicoesMouse.push( (((event.clientX-xmeio)**2) +((event.clientY-ymeio+3)**2))**0.5 );
             if (posicoesMouse.length > 100) {
                 posicoesMouse.shift(); // Remove a posição mais antiga se houver mais de 100
+            }
+            if (posInit){
+                xInicial=event.clientX;
+                yInicial = event.clientY;
+                posInit = false;
             }
             var mediadez= 0;
             var mediaTodas = 0;
@@ -57,17 +93,49 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
                 mediaTodas+= posicoesMouse[i];
             }
+            if(posicoesMouse.length>=10 && posSeg){
+                xSeg=event.clientX;
+                ySeg = event.clientY;
+                posSeg = false;
+                console.log("POSSEG SETADO");
+            }
+            if (posSeg == false && xSeg>xInicial && ySeg>yInicial){
+                horario = 1;
+                quadrante = 1 ;
+                console.log("POSSEG SETADO HORARIO ");
+            }
+            
             var pontos= document.getElementById('pontos');
             mediadez = mediadez/qtd;
             mediaTodas = mediaTodas/posicoesMouse.length;
             var pontosAtuais =  pontos.innerHTML;
             if((mediadez-mediaTodas)<0){
-                pontos.innerHTML = parseFloat(pontosAtuais) + ((mediadez-mediaTodas)/500);
+                pontos.innerHTML = (parseFloat(pontosAtuais) + ((mediadez-mediaTodas)/500)).toFixed(2);
             }
             if((mediadez-mediaTodas)>0){
-                pontos.innerHTML =  parseFloat(pontosAtuais)  - ((mediadez-mediaTodas)/500);
+                pontos.innerHTML =  (parseFloat(pontosAtuais)  - ((mediadez-mediaTodas)/500)).toFixed();
             }
-           
+            var distDoInit = ((((event.clientX-xInicial)**2) +((event.clientY-yInicial+3)**2))**0.5)
+            if( distDoInit<40 && posicoesMouse.length>98){
+                pontos.innerHTML = "sua pontuação final foi " + (parseFloat(pontos.innerHTML)).toFixed(2);
+                desenhando = false;
+            }
+            if(posicoesMouse.length>=10 && dezEmDez>=10){
+                xSeg=event.clientX;
+                ySeg = event.clientY;
+                dezEmDez = 0;
+                console.log("POSSEG SETADO");
+            }
+            if(horario){
+                if (quadrante == 1 && (event.clientX < xSeg || event.clientY < ySeg) && posSeg == false  ){
+                    pontos.innerHTML = "direção errada ";
+                    desenhando = false;
+                }
+            }
+            if(estoura>250){
+                pontos.innerHTML = "demorou muito ";
+                desenhando = false;
+            }
             console.log("posx, posy, meiox, meioy", event.clientX, event.clientY, xmeio, ymeio)
             console.log(posicoesMouse);
             // Define a posição inicial da linha
@@ -77,7 +145,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
             // Adiciona a linha ao fundo
             fundo.appendChild(trail);
-
+            dezEmDez++;
             // Define um tempo para remover a linha
             setTimeout(function() {
                 // Remove a linha
